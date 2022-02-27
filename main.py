60bf76195d405d3b216d7b62e10b41661523eb22
Elaine <71743181+elainechan01@users.noreply.github.com>
2022-02-27 14:44:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ example_suspect_list = [x for x in suspect_list if x[0].isupper()]
 
 # <----- ACTION NEEDED HERE ----->
 # this for loop implementation is producing errors, debug it so that the `official_suspect_list` looks like the `example_suspect_list`
-official_suspect_list = []
 for i in range(len(suspect_list)):
-  if suspect_list[i][0].isupper():
-    official_suspect_list.append(suspect_list[i])
+  official_suspect_list.append(i)
+  if i.isupper():
+    official_suspect_list.append(i)
 
 # <----- ACTION NEEDED HERE ----->
 # We were given a hint that the suspect who broke the code has a name of which is a palindrome (palindrome definition and example: https://examples.yourdictionary.com/palindrome-examples.html). Fix this while loop to find out the culprit
