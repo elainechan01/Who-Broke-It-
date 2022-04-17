d9b28bc9dc58a054c3e52cac66637635f00a4a7f
Elaine <71743181+elainechan01@users.noreply.github.com>
2022-04-17 16:47:50 -0400

    Update main.py
    - official_suspect_list init
    - for loop function

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ example_suspect_list = [x for x in suspect_list if x[0].isupper()]
 
 # <----- ACTION NEEDED HERE ----->
 # this for loop implementation is producing errors, debug it so that the `official_suspect_list` looks like the `example_suspect_list`
-for i in range(len(suspect_list)):
+official_suspect_list = []
+for i in suspect_list:
     if i[0].isupper():
         official_suspect_list.append(suspect_list[i])
 
 # <----- ACTION NEEDED HERE ----->
 # We were given a hint about the length of the culprit's name (It can be found in the helper file called `anonymous.py`). Help us fix this while loop to find out the culprit.
 index = 0
-culprit = None
+culprit = ""
 while index < len(official_suspect_list):
     curr = official_suspect_list[index]
     ptr = 0
